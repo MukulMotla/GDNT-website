d291ad751b72a277036bca7fc113632039396bab
abc <abc@abcs-MacBook-Pro.local>
2025-12-16 13:41:51 +0530

    Fix Router config for Netlify (remove basename & HashRouter)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
@@ -10,7 +10,7 @@ import ProductDetails from "./pages/ProductDetails";
 
 function App() {
   return (
-    <Router basename="/GDNT-website">
+    <Router>
       <div className="relative min-h-screen flex flex-col">
         <NavBar />
         <MessageBox />
